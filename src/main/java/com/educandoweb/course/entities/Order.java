f7abb8c8c16e3edb92ac71436ff53894beaa038f
Ericris Rossato <rossato.ep@gmail.com>
2023-01-15 18:54:42 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -102,6 +102,15 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double sum = 0d;
+		
+		for(OrderItem x : items) {
+			sum += x.getSubtotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
